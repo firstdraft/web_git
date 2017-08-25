83e0292ea8ffe50380e246298a2571534fd6a677
Murugan <murugu2012@gmail.com>
2017-08-25 08:59:51 +0530

    mount routes automatically (#8)

--- lib/web_git/engine.rb
+++ lib/web_git/engine.rb
@@ -1,5 +1,9 @@
 module WebGit
   class Engine < ::Rails::Engine
     isolate_namespace WebGit
+
+    initializer 'web_git.routes' do |app|
+      app.routes.append { mount WebGit::Engine, at: "/git" }
+    end
   end
 end
