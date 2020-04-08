9112c93d8cc58f51d692bc55c4d5e99ddddfe659
Jelani Woods <jelani@firstdraft.com>
2020-04-08 12:24:52 -0500

    fix deployment issue

--- lib/generators/web_git/install_generator.rb
+++ lib/generators/web_git/install_generator.rb
@@ -5,10 +5,16 @@ module WebGit
       log :insert, "Updating config.ru to run apps in parallel."
       
       contents = <<-RUBY.gsub(/^      /, "")
-      map '/git' do
-        run WebGit::Server
+      # This file is used by Rack-based servers to start the application.
+
+      require_relative 'config/environment'
+
+      if Rails.env.development?
+        map '/git' do
+          run WebGit::Server
+        end
       end
-      
+
       map '/' do
         run Rails.application
       end
