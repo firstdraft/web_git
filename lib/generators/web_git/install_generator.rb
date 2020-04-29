361b56fba1626bac76da0537893270609d9c4b7b
Jelani Woods <jelani@firstdraft.com>
2020-04-29 15:49:33 -0500

    Make installer reversible.

--- lib/generators/web_git/install_generator.rb
+++ lib/generators/web_git/install_generator.rb
@@ -5,9 +5,6 @@ module WebGit
       log :insert, "Updating config.ru to run apps in parallel."
       
       contents = <<-RUBY.gsub(/^      /, "")
-      # This file is used by Rack-based servers to start the application.
-
-      require_relative 'config/environment'
 
       if Rails.env.development?
         map '/git' do
@@ -15,31 +12,14 @@ module WebGit
         end
       end
 
-      map '/' do
-        run Rails.application
-      end
+      map '/' do\n\t
       RUBY
 
       filename = "config.ru"
       match_text = "run Rails.application"
 
-      # insert_into_file filename, before: match_text do contents
-      # end
-
-      # gsub_file filename, match_text, "\t" + match_text
-      gsub_file filename, match_text, contents
-
-      # append_config
-      
-      # append_file filename do "\nend"
-      # end
-    end
-    
-    private 
-
-    def append_config
-      append_file "config.ru" do "\nend"
-      end
+      insert_into_file filename, contents, before: match_text
+      insert_into_file filename, "\nend", after: match_text, force: true
     end
   end
 end
