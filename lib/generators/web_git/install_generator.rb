495b5e51562660d943b28d8d56f2ecc02514903b
Jelani Woods <jelani@firstdraft.com>
2021-07-12 15:44:48 -0500

    Allow installer to run multiple times (#111)
    Currently when the installer is run multiple times, additional ends or other Ruby is injected into config.ru which causes app breaking errors.
    
    This commit updates the installer to conditionally modify config.ru when it doesn't contain the required Ruby for web_git and allows for the installer to be run multiple times without issue.

--- lib/generators/web_git/install_generator.rb
+++ lib/generators/web_git/install_generator.rb
@@ -2,32 +2,46 @@ module WebGit
   class InstallGenerator < Rails::Generators::Base
     def generate_server
       
+      filename = Rails.root.join("config.ru")
+
       log :insert, "Updating config.ru to run apps in parallel."
-      
-      contents = <<-RUBY.gsub(/^      /, "")
+      if File.exists?(filename) && already_installed?
+        log :identical, "Skipping overrides."
+      else
+        contents = <<~RUBY
 
-      if Rails.env.development?
-        map '/git' do
-          run WebGit::Server
+        if Rails.env.development?
+          map '/git' do
+            run WebGit::Server
+          end
         end
+  
+        map '/' do
+        RUBY
+  
+        filename = "config.ru"
+        match_text = "run Rails.application"
+  
+        insert_into_file filename, contents, before: match_text
+        insert_into_file filename, "\nend", after: match_text, force: true
+        gsub_file filename, match_text, "\t#{match_text}"
       end
-
-      map '/' do
-      RUBY
-
-      filename = "config.ru"
-      match_text = "run Rails.application"
-
-      insert_into_file filename, contents, before: match_text
-      insert_into_file filename, "\nend", after: match_text, force: true
-      gsub_file filename, match_text, "\t#{match_text}"
 
       expect_installed = run("which expect")
 
       unless expect_installed
         log :insert, "Installing expect."
         run "sudo apt install -y expect"
+      else
+        log :identical, "expect already installed."
       end
+    end
+
+    def already_installed?
+      filename = Rails.root.join("config.ru")
+      contents = open(filename).read
+
+      contents.match?(/if\s*Rails.env.development\?\s*map\s*'\/git'\s*do\s*run\s*WebGit::Server\s*end\s*end/)
     end
   end
 end
